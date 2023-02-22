cfd8b8527364b780f0774c35f53c668fd4b3cd98
Sebastian <isebasus@gmail.com>
2023-02-21 21:27:24 -0800

    bruh momentum

--- src/components/Slider/components/Feature/index.tsx
+++ src/components/Slider/components/Feature/index.tsx
@@ -5,9 +5,9 @@ export const Feature = (props: IFeature) => {
     return (
         <div className="grid grid-cols-2">
             <div className="details">
-                <h1>{title}</h1>
+                <h1 className="background-clip font-display text-m-h1 sm:text-d-h2 text-3xl md:text-5xl lg:text-[length:64px] xl:text-d-j font-black"><span className="title">{title}</span></h1>
                 <div className="h-5" />
-                <h2>{description}</h2>
+                <h2 className="text-custom-gray-300">{description}</h2>
                 <button onClick={() => console.log(id)}>More Details</button>
             </div>
             <div className="image-holder">
