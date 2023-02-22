a43c8bf494afb0e0577268239178826eb199f696
Sebastian <isebasus@gmail.com>
2023-02-21 20:27:19 -0800

    bruh

--- src/components/Slider/components/Feature/index.tsx
+++ src/components/Slider/components/Feature/index.tsx
@@ -4,13 +4,13 @@ export const Feature = (props: IFeature) => {
     const { id, title, description, image } = props;
     return (
         <div className="grid grid-cols-2">
-            <div>
+            <div className="details">
                 <h1>{title}</h1>
                 <div className="h-5" />
                 <h2>{description}</h2>
                 <button onClick={() => console.log(id)}>More Details</button>
             </div>
-            <div>
+            <div className="image-holder">
                 <img
                     className="object-contain h-48 w-11"
                     src={image}
